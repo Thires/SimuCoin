0aaa772a91b718a879e3aad848ca17492a264e56
Thires <thiresdr@gmail.com>
2023-03-05 05:13:42 -0500

    Add files via upload

---
+++ mainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace SimuCoin
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.loginButton = new System.Windows.Forms.Button();
+            this.signoutButton = new System.Windows.Forms.Button();
+            this.claimButton = new System.Windows.Forms.Button();
+            this.currentCoinsLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.userNameTB = new System.Windows.Forms.TextBox();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(8, 107);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(86, 24);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // signoutButton
+            // 
+            this.signoutButton.Location = new System.Drawing.Point(215, 107);
+            this.signoutButton.Name = "signoutButton";
+            this.signoutButton.Size = new System.Drawing.Size(86, 24);
+            this.signoutButton.TabIndex = 3;
+            this.signoutButton.Text = "Signout";
+            this.signoutButton.UseVisualStyleBackColor = true;
+            this.signoutButton.Click += new System.EventHandler(this.signoutButton_Click);
+            // 
+            // claimButton
+            // 
+            this.claimButton.Location = new System.Drawing.Point(110, 107);
+            this.claimButton.Name = "claimButton";
+            this.claimButton.Size = new System.Drawing.Size(86, 24);
+            this.claimButton.TabIndex = 5;
+            this.claimButton.Text = "Claim";
+            this.claimButton.UseVisualStyleBackColor = true;
+            this.claimButton.Visible = false;
+            this.claimButton.Click += new System.EventHandler(this.claimButton_Click);
+            // 
+            // currentCoinsLabel
+            // 
+            this.currentCoinsLabel.AutoSize = true;
+            this.currentCoinsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.currentCoinsLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.currentCoinsLabel.ForeColor = System.Drawing.Color.White;
+            this.currentCoinsLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.currentCoinsLabel.Location = new System.Drawing.Point(64, 26);
+            this.currentCoinsLabel.Name = "currentCoinsLabel";
+            this.currentCoinsLabel.Size = new System.Drawing.Size(108, 25);
+            this.currentCoinsLabel.TabIndex = 7;
+            this.currentCoinsLabel.Text = "You have";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timeLabel.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(8, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(206, 16);
+            this.timeLabel.TabIndex = 11;
+            this.timeLabel.Text = "Next Subscription Bonus in:";
+            // 
+            // userNameTB
+            // 
+            this.userNameTB.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.userNameTB.Location = new System.Drawing.Point(8, 77);
+            this.userNameTB.Name = "userNameTB";
+            this.userNameTB.Size = new System.Drawing.Size(114, 23);
+            this.userNameTB.TabIndex = 13;
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Location = new System.Drawing.Point(186, 77);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.PasswordChar = '*';
+            this.passwordTB.Size = new System.Drawing.Size(114, 23);
+            this.passwordTB.TabIndex = 15;
+            this.passwordTB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordTB_KeyDown);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.BackColor = System.Drawing.Color.Transparent;
+            this.statusLabel.ForeColor = System.Drawing.Color.White;
+            this.statusLabel.Location = new System.Drawing.Point(8, 136);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(51, 16);
+            this.statusLabel.TabIndex = 17;
+            this.statusLabel.Text = "Status";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(23, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "UserName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(215, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Password";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(307, 162);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.passwordTB);
+            this.Controls.Add(this.userNameTB);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.currentCoinsLabel);
+            this.Controls.Add(this.claimButton);
+            this.Controls.Add(this.signoutButton);
+            this.Controls.Add(this.loginButton);
+            this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SimuCoins";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button loginButton;
+        private Button signoutButton;
+        private Button claimButton;
+        private Label currentCoinsLabel;
+        private Label label2;
+        private Label timeLabel;
+        private TextBox userNameTB;
+        private TextBox passwordTB;
+        private Label statusLabel;
+        private Label label1;
+        private Label label3;
+    } 
+}
---
+++ mainForm.cs
@@ -0,0 +1,139 @@
+﻿using GeniePlugin.Interfaces;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SimuCoin
+{
+    public partial class mainForm : Form
+    {
+        private CookieContainer cookies = new CookieContainer();
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string url = "https://store.play.net/Account/SignIn?returnURL=%2FAccount%2FSignIn";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.CookieContainer = cookies;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            string pageContent = string.Empty;
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    pageContent = reader.ReadToEnd();
+                }
+            }
+
+            string token = Regex.Match(pageContent, "<input name=\"__RequestVerificationToken\" type=\"hidden\" value=\"(.*?)\" />").Groups[1].Value;
+
+            string username = userNameTB.Text;
+            string password = passwordTB.Text;
+
+            string postData = string.Format("__RequestVerificationToken={0}&UserName={1}&Password={2}&RememberMe=true", token, username, password);
+
+            request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.CookieContainer = cookies;
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = postData.Length;
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(postData);
+                }
+            }
+
+            response = (HttpWebResponse)request.GetResponse();
+
+            pageContent = string.Empty;
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    pageContent = reader.ReadToEnd();
+                }
+            }
+
+            if (response.ResponseUri.ToString() == "https://store.play.net/")
+            {
+                statusLabel.Text = "Login successful";
+                UpdateBalance();
+            }
+            else
+            {
+                statusLabel.Text = "Login failed";
+            }
+        }
+
+        private void UpdateBalance()
+        {
+            string url = "https://store.play.net/store/purchase/dr";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.CookieContainer = cookies;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            string pageContent = string.Empty;
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    pageContent = reader.ReadToEnd();
+                }
+            }
+
+
+            string time = Regex.Match(pageContent, "<h1\\s+class=\"RewardMessage\\s+centered\\s+sans_serif\">Next Subscription Bonus in\\s+(.*?)</h1>").Groups[1].Value;
+            timeLabel.Text = "Next Subscription Bonus in " + time;
+
+            string balance = Regex.Match(pageContent, "<span class=\"blue\" id=\"side_balance\">(.*?)</span>").Groups[1].Value;
+            currentCoinsLabel.Text = "You Have " + balance;
+
+            string claim = Regex.Match(pageContent, "< h1 class=\"RewardMessage centered sans_serif\">Claimed (.*?) SimuCoin reward!</h1>").Groups[1].Value;
+            claimButton.Visible = true;
+            claimButton.Text = "Claim Reward!";
+        }
+
+        private void signoutButton_Click(object sender, EventArgs e)
+        {
+            string url = "https://store.play.net/Account/SignOut";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.CookieContainer = cookies;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            statusLabel.Text = "Signed out";
+        }
+
+        private void passwordTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            var capsLockOn = Control.IsKeyLocked(Keys.CapsLock);
+            statusLabel.Text = $"Caps Lock is {(capsLockOn ? "on" : "off")}.";
+        }
+
+        private void claimButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not implemented");
+        }
+    }
+}
